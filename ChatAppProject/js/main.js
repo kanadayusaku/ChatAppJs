f3ef8fe2e7e8b2c3a203c6558d533461706f6162
Kanada <yamada@mail.com>
2019-09-16 11:07:55 +0900

    ChatApp

---
+++ ChatAppProject/js/main.js
@@ -0,0 +1,114 @@
+(() => {
+  'use strict';
+
+  <!-- TODO: Add SDKs for Firebase products that you want to use
+       https://firebase.google.com/docs/web/setup#config-web-app -->
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyAnRFPnokTApbsKvVt7-ouXyLYoCJ5UUEc",
+      authDomain: "mychatapp-5e516.firebaseapp.com",
+      databaseURL: "https://mychatapp-5e516.firebaseio.com",
+      projectId: "mychatapp-5e516",
+      storageBucket: "mychatapp-5e516.appspot.com",
+      messagingSenderId: "407035234576",
+      appId: "1:407035234576:web:8c822217b8a38bc9b1fd95"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+   const db = firebase.firestore();
+
+   //ドットインストールで書けと言われた箇所
+   // db.settings({ timestampsInSnapshots: true })
+
+  const collection = db.collection('messages');
+
+  const auth = firebase.auth();
+  //ユーザー情報の保持
+  let me = null;
+
+  const message = document.getElementById('message');
+  const form = document.querySelector('form');
+  const messages = document.getElementById('messages');
+
+  const login = document.getElementById('login');
+  const logout = document.getElementById('logout');
+
+
+
+  login.addEventListener('click', () => {
+    auth.signInAnonymously();
+  });
+
+  logout.addEventListener('click', () => {
+    auth.signOut();
+  });
+
+  auth.onAuthStateChanged(user => {
+    if(user) {
+      me = user;
+
+   while (messages.firstChild) {
+     messages.removeChild(messages.firstChild);
+   }
+
+      //collectionの変更
+      collection.orderBy('created').onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type === 'added') {
+            const li = document.createElement('li');
+            const d = change.doc.data();
+            li.textContent = d.uid.substr(0, 8) + ': ' + d.message;
+            messages.appendChild(li);
+          }
+        });
+      }, error => {});
+      console.log(`Logged in as: ${user.uid}`);
+      login.classList.add('hidden');
+      [logout, form, messages].forEach(el => {
+        el.classList.remove('hidden');
+      });
+      message.focus();
+      return;
+    }
+    me = null;
+    console.log('Nobody is logged in');
+    login.classList.remove('hidden');
+    [logout, form, messages].forEach(el => {
+      el.classList.add('hidden');
+    });
+  });
+
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const val = message.value.trim();
+    if (val === "") {
+      return;
+    }
+
+    //投稿直後に要素を追加
+    // const li = document.createElement('li');
+    // li.textContent = val;
+    // messages.appendChild(li);
+
+    message.value = '';
+    message.focus();
+
+    collection.add({
+      message: val,
+      created: firebase.firestore.FieldValue.serverTimestamp(),
+      uid: me ? me.uid : 'nobody'
+    })
+   .then(doc => {
+     console.log(`${doc.id} added!`)
+
+   })
+   .catch(error => {
+     console.log('document add error!');
+     console.log(error);
+   });
+  });
+})();
